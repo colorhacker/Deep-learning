ddd6278438b8584c19f8bb7b077d175d850c2f66
liting <2863515010@qq.com>
2019-04-10 15:21:01 +0800

    update

--- main_app/other/rgb_hsl.cpp
+++ main_app/other/rgb_hsl.cpp
@@ -1,19 +1,7 @@
 
 #include "rgb_hsl.h"
 
-/**
-* Converts an HSL color value to RGB. Conversion formula
-* adapted from http://en.wikipedia.org/wiki/HSL_color_space.
-* Assumes h, s, and l are contained in the set [0, 1] and
-* returns r, g, and b in the set [0, 255].
-*
-* @param   {number}  h       The hue
-* @param   {number}  s       The saturation
-* @param   {number}  l       The lightness
-* @return  {Array}           The RGB representation
-*/
-
-static float hue_to_rgb(float p, float q, float t) {
+static float _hue_to_rgb(float p, float q, float t) {
 	if (t < 0.0f) t += 1.0f;
 	if (t > 1.0f) t -= 1.0f;
 	if (t < 1.0f / 6.0f) return (p + (q - p) * 6 * t);
@@ -21,22 +9,17 @@ static float hue_to_rgb(float p, float q, float t) {
 	if (t < 2.0f / 3.0f) return (p + (q - p) * (2 / 3 - t) * 6);
 	return p;
 }
-
-void HSL_to_RGB(user_nn_hsl *hsl, user_nn_rgb *rgb) {
-	user_nn_hsl rgb_f = { 0.0f, 0.0f, 0.0f };
-	if (hsl->S == 0) {
-		rgb_f.H = rgb_f.S = rgb_f.L = hsl->L; // achromatic
-	}
-	else {
-		float q = hsl->L < 0.5f ? hsl->L * (1.0f + hsl->S) : hsl->L + hsl->S - hsl->L * hsl->S;
-		float p = 2.0f * hsl->L - q;
-		rgb_f.H = hue_to_rgb(p, q, hsl->H + 1.0f / 3.0f);
-		rgb_f.S = hue_to_rgb(p, q, hsl->H);
-		rgb_f.L = hue_to_rgb(p, q, hsl->H - 1.0f / 3.0f);
-	}
-	rgb->R = (unsigned char)(rgb_f.H * 255);
-	rgb->G = (unsigned char)(rgb_f.S * 255);
-	rgb->B = (unsigned char)(rgb_f.L * 255);
+static float _max_rgb(float *rgb) {
+	float max = rgb[0];
+	max = max < rgb[1] ? rgb[1] : max;
+	max = max < rgb[2] ? rgb[2] : max;
+	return max;
+}
+static float _min_rgb(float *rgb) {
+	float min = rgb[0];
+	min = min > rgb[1] ? rgb[1] : min;
+	min = min > rgb[2] ? rgb[2] : min;
+	return min;
 }
 
 /**
@@ -50,21 +33,10 @@ void HSL_to_RGB(user_nn_hsl *hsl, user_nn_rgb *rgb) {
 * @param   {number}  b       The blue color value
 * @return  {Array}           The HSL representation
 */
-static float _max_rgb(user_nn_hsl *rgb) {
-	float max = rgb->H;
-	max = max < rgb->S ? rgb->S : max;
-	max = max < rgb->L ? rgb->L : max;
-	return max;
-}
-static float _min_rgb(user_nn_hsl *rgb) {
-	float min = rgb->H;
-	min = min > rgb->S ? rgb->S : min;
-	min = min > rgb->L ? rgb->L : min;
-	return min;
-}
+
 void RGB_to_HSL(user_nn_rgb *rgb, user_nn_hsl *hsl) {
-	user_nn_hsl rgb_f = { (float)rgb->R / 255, (float)rgb->G / 255, (float)rgb->B / 255};
-	float max = _max_rgb(&rgb_f), min = _min_rgb(&rgb_f);
+	float rgb_f[] = { (float)rgb->R / 255, (float)rgb->G / 255, (float)rgb->B / 255};
+	float max = _max_rgb(rgb_f), min = _min_rgb(rgb_f);
 	hsl->L = (max + min) / 2;
 	if (max == min) {
 		hsl->H = hsl->S = 0; // achromatic
@@ -72,17 +44,44 @@ void RGB_to_HSL(user_nn_rgb *rgb, user_nn_hsl *hsl) {
 	else {
 		float d = max - min;
 		hsl->S = hsl->L > 0.5f ? d / (2.0f - max - min) : d / (max + min);
-		if (max == rgb_f.H) {
-			hsl->H = (rgb_f.S - rgb_f.L) / d + (rgb_f.S < rgb_f.L ? 6 : 0);
-		}else if (max == rgb_f.S) {
-			hsl->H = (rgb_f.L - rgb_f.H) / d + 2;
-		}else if (max == rgb_f.L) {
-			hsl->H = (rgb_f.H - rgb_f.S) / d + 4;
+		if (max == rgb_f[0]) {
+			hsl->H = (rgb_f[1] - rgb_f[2]) / d + (rgb_f[1]  < rgb_f[2] ? 6 : 0);
+		}else if (max == rgb_f[1]) {
+			hsl->H = (rgb_f[2] - rgb_f[0]) / d + 2;
+		}else if (max == rgb_f[2]) {
+			hsl->H = (rgb_f[0] - rgb_f[1]) / d + 4;
 		}
 		hsl->H /= 6;
 	}
 }
+/**
+* Converts an HSL color value to RGB. Conversion formula
+* adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+* Assumes h, s, and l are contained in the set [0, 1] and
+* returns r, g, and b in the set [0, 255].
+*
+* @param   {number}  h       The hue 色相分量
+* @param   {number}  s       The saturation 颜色饱和度
+* @param   {number}  l       The lightness 颜色亮度
+* @return  {Array}           The RGB representation
+*/
 
+void HSL_to_RGB(user_nn_hsl *hsl, user_nn_rgb *rgb) {
+	user_nn_hsl rgb_f = { 0.0f, 0.0f, 0.0f };
+	if (hsl->S == 0) {
+		rgb_f.H = rgb_f.S = rgb_f.L = hsl->L; // achromatic
+	}
+	else {
+		float q = hsl->L < 0.5f ? hsl->L * (1.0f + hsl->S) : hsl->L + hsl->S - hsl->L * hsl->S;
+		float p = 2.0f * hsl->L - q;
+		rgb_f.H = _hue_to_rgb(p, q, hsl->H + 1.0f / 3.0f);
+		rgb_f.S = _hue_to_rgb(p, q, hsl->H);
+		rgb_f.L = _hue_to_rgb(p, q, hsl->H - 1.0f / 3.0f);
+	}
+	rgb->R = (unsigned char)(rgb_f.H * 255);
+	rgb->G = (unsigned char)(rgb_f.S * 255);
+	rgb->B = (unsigned char)(rgb_f.L * 255);
+}
 /**
 * Converts an RGB color value to HSV. Conversion formula
 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
@@ -94,25 +93,25 @@ void RGB_to_HSL(user_nn_rgb *rgb, user_nn_hsl *hsl) {
 * @param   Number  b       The blue color value
 * @return  Array           The HSV representation
 */
-void RGB_to_HSL(user_nn_rgb *rgb, user_nn_hsl *hsv) {
-	user_nn_hsl rgb_f = { (float)rgb->R / 255, (float)rgb->G / 255, (float)rgb->B / 255 };
-	float max = _max_rgb(&rgb_f), min = _min_rgb(&rgb_f);
-	hsv->L = (max + min) / 2;
+void RGB_to_HSV(user_nn_rgb *rgb, user_nn_hsv *hsv) {
+	float rgb_f[] = { (float)rgb->R / 255, (float)rgb->G / 255, (float)rgb->B / 255 };
+	float max = _max_rgb(rgb_f), min = _min_rgb(rgb_f);
 	float d = max - min;
+	hsv->V = max;
 	hsv->S = max == 0.0f ? 0.0f : d / max;
 
 	if (max == min) {
 		hsv->H = 0; // achromatic
 	}
 	else {
-		if (max == rgb_f.H) {
-			hsv->H = (rgb_f.S - rgb_f.L) / d + (rgb_f.S < rgb_f.L ? 6 : 0);
+		if (max == rgb_f[0]) {
+			hsv->H = (rgb_f[1] - rgb_f[2]) / d + (rgb_f[1] < rgb_f[2] ? 6.0f : 0.0f);
 		}
-		else if (max == rgb_f.S) {
-			hsv->H = (rgb_f.L - rgb_f.H) / d + 2;
+		else if (max == rgb_f[1]) {
+			hsv->H = (rgb_f[2] - rgb_f[0]) / d + 2.0f;
 		}
-		else if (max == rgb_f.L) {
-			hsv->H = (rgb_f.H - rgb_f.S) / d + 4;
+		else if (max == rgb_f[2]) {
+			hsv->H = (rgb_f[0] - rgb_f[1]) / d + 4.0f;
 		}
 		hsv->H /= 6;
 	}
@@ -123,28 +122,31 @@ void RGB_to_HSL(user_nn_rgb *rgb, user_nn_hsl *hsv) {
 * Assumes h, s, and v are contained in the set [0, 1] and
 * returns r, g, and b in the set [0, 255].
 *
-* @param   Number  h       The hue
-* @param   Number  s       The saturation
-* @param   Number  v       The value
+* @param   Number  h       The hue 色调
+* @param   Number  s       The saturation 饱和度 
+* @param   Number  v       The value 明度
 * @return  Array           The RGB representation
 */
-function hsvToRgb(h, s, v) {
-	var r, g, b;
+void HSV_to_RGB(user_nn_hsv *hsv, user_nn_rgb *rgb) {
+	user_nn_hsv rgb_f = {0.0f,0.0f,0.0f};
 
-	var i = Math.floor(h * 6);
-	var f = h * 6 - i;
-	var p = v * (1 - s);
-	var q = v * (1 - f * s);
-	var t = v * (1 - (1 - f) * s);
+	int i = (int)floor(hsv->H * 6);
+	float f = hsv->H * 6 - i;
+	float p = hsv->V * (1 - hsv->S);
+	float q = hsv->V * (1 - f * hsv->S);
+	float t = hsv->V * (1 - (1 - f) * hsv->S);
 
 	switch (i % 6) {
-	case 0: r = v, g = t, b = p; break;
-	case 1: r = q, g = v, b = p; break;
-	case 2: r = p, g = v, b = t; break;
-	case 3: r = p, g = q, b = v; break;
-	case 4: r = t, g = p, b = v; break;
-	case 5: r = v, g = p, b = q; break;
+		case 0: rgb_f.H = hsv->V, rgb_f.S = t, rgb_f.V = p; break;
+		case 1: rgb_f.H = q, rgb_f.S = hsv->V, rgb_f.V = p; break;
+		case 2: rgb_f.H = p, rgb_f.S = hsv->V, rgb_f.V = t; break;
+		case 3: rgb_f.H = p, rgb_f.S = q, rgb_f.V = hsv->V; break;
+		case 4: rgb_f.H = t, rgb_f.S = p, rgb_f.V = hsv->V; break;
+		case 5: rgb_f.H = hsv->V, rgb_f.S = p, rgb_f.V = q; break;
+		default:break;
 	}
 
-	return[r * 255, g * 255, b * 255];
+	rgb->R = (unsigned char)(rgb_f.H * 255);
+	rgb->G = (unsigned char)(rgb_f.S * 255);
+	rgb->B = (unsigned char)(rgb_f.V * 255);
 }
--- main_app/other/rgb_hsl.h
+++ main_app/other/rgb_hsl.h
@@ -1,9 +1,10 @@
-#ifndef _rgb_hsl_H
-#define _rgb_hsl_H
+#ifndef _rgb_hs__H
+#define _rgb_hs__H
 
 #include<stdio.h>
 #include<stdlib.h> 
 #include<string.h>
+#include <math.h>
 
 typedef struct _nn_rgb {
 	unsigned char R;
@@ -17,7 +18,15 @@ typedef struct _nn_hsl {
 	float L;
 }user_nn_hsl;
 
-void HSL_to_RGB(user_nn_hsl *hsl, user_nn_rgb *rgb);
+typedef struct _nn_hsv {
+	float H;
+	float S;
+	float V;
+}user_nn_hsv;
+
 void RGB_to_HSL(user_nn_rgb *rgb, user_nn_hsl *hsl);
+void HSL_to_RGB(user_nn_hsl *hsl, user_nn_rgb *rgb);
+void RGB_to_HSV(user_nn_rgb *rgb, user_nn_hsv *hsv);
+void HSV_to_RGB(user_nn_hsv *hsv, user_nn_rgb *rgb);
 
 #endif
