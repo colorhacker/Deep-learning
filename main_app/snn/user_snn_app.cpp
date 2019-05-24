2949d1035a8ce280fd36bc85824425ab3ee7d225
liting <2863515010@qq.com>
2019-05-24 17:52:29 +0800

    update

--- main_app/snn/user_snn_app.cpp
+++ main_app/snn/user_snn_app.cpp
@@ -30,22 +30,8 @@ void user_snn_app_train(int argc, const char** argv) {
 		printf("loading model failed\ncreate nn new object \n");
 		snn_layers = user_snn_model_create(user_layers);//创建模型
 	}
-	//user_nn_matrix *src_matrix = user_nn_matrix_create(1, 2);//卷积矩阵
-	//src_matrix->data[0] = 1.5f;
-	//src_matrix->data[1] = 0.5f;
-	//for (;;) {
-	//	for (int train_index = 0; train_index < 1000; train_index++) {
-	//		user_snn_model_load_input_feature(snn_layers, src_matrix);//加载输入数据
-	//		user_snn_model_load_target_feature(snn_layers, src_matrix);//加载目标数据	
-	//		user_snn_model_ffp(snn_layers);
-	//		user_snn_model_bp(snn_layers);
-	//	}
-	//	break;
-	//}
-	//user_snn_model_ffp(snn_layers);
-	//user_snn_output_layers *snn_output_layer = (user_snn_output_layers *)user_snn_model_return_layer(snn_layers, u_snn_layer_type_output)->content;
-	//user_nn_matrix_printf(NULL, snn_output_layer->feature_matrix);
 	int info = 0;
+	clock_t start_time = clock();
 	for (;;) {
 		for (int train_index = 0; train_index < train_images->height * train_images->width; train_index++) {
 			user_snn_model_load_input_feature(snn_layers, user_nn_matrices_ext_matrix_index(train_images, train_index));//加载输入数据
@@ -59,24 +45,25 @@ void user_snn_app_train(int argc, const char** argv) {
 		}
 		break;
 	}
+	printf("\ntime:%d", (clock() - start_time) / 1000);
+	start_time = clock();
 	float success = 0;
 	for (;;) {
 		for (int test_index = 0; test_index < test_images->height * test_images->width; test_index++) {
 			user_snn_model_load_input_feature(snn_layers, user_nn_matrices_ext_matrix_index(test_images, test_index));//加载输入数据
 			user_snn_model_load_target_feature(snn_layers, user_nn_matrices_ext_matrix_index(test_lables, test_index));//加载目标数据	
 			user_snn_model_ffp(snn_layers);
-
 			if (*user_nn_matrix_return_max_addr(user_snn_model_return_result(snn_layers)) == *user_nn_matrix_return_max_addr(user_nn_matrices_ext_matrix_index(test_lables, test_index))) {
 				success++;
 			}
 			if (info++ > 1000) {
 				info = 0;
-				printf("\n%d", test_index);
+				//printf("\n%d", test_index);
 			}
 		}
 		break;
 	}
-	printf("\n%.4f\n", 100 * success /(float)(test_images->height * test_images->width));
+	printf("\nsuccess:%.4f,time:%d\n", 100 * success /(float)(test_images->height * test_images->width), (clock() - start_time) / 1000);
 	system("pause");
 }
 void user_snn_app_ident(int argc, const char** argv) {
