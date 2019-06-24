87ba29a5790d238a98d87311e14ea6ca484d0775
liting <2863515010@qq.com>
2019-06-24 18:33:06 +0800

    update

--- main_app/python/index.py
+++ main_app/python/index.py
@@ -17,7 +17,7 @@ def display_image(data):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def mnsit_feature_data(c_w,c_h,s=1):
+def mnsit_feature_data(c_w,c_h,s):
     images, labels = load_mnist()
     result = np.empty(shape=[0,c_w*c_h], dtype=int)
     #for i in range(len(images)):
@@ -28,30 +28,35 @@ def mnsit_feature_data(c_w,c_h,s=1):
             for y in range(data.shape[1]-c_h+1):
                 #print(data[x:x+c_w,y:y+c_h].flatten())
                 result = np.row_stack((result, data[x:x+c_w,y:y+c_h].flatten()))
-                y = y+s
-            x = x+s
+                y = y + s
+            x = x + s
     return result
 
 def kmeans_feature(data,n_class):
     kmeans = KMeans(n_clusters=n_class).fit(data)
     #y_kmeans = kmeans.predict(data)
-    return kmeans.cluster_centers_ # kmeans.labels_ # kmeans.inertia_
+    return kmeans,kmeans.cluster_centers_ # kmeans.labels_ # kmeans.inertia_
 
-def rebuild_mnist(kmeans,data,c_w,c_h,s=1):
+def rebuild_mnist(kmeans,data,c_w,c_h,s):
+    result = np.empty(shape=[0, c_w * c_h], dtype=int)
     for x in range(data.shape[0] - c_w + 1):
+        x = x + s
         for y in range(data.shape[1] - c_h + 1):
+            y = y + s
             #print(data[x:x+c_w,y:y+c_h].flatten())
             #data[x:x + c_w, y:y + c_h].flatten()
-            print(kmeans.predict(data[x:x+c_w,y:y+c_h].flatten()))
+            #print(kmeans.predict(data[x:x+c_w,y:y+c_h].flatten()))
             #print(kmeans.fit_predict(data[x:x+c_w,y:y+c_h].flatten()))
-            y = y + s
-        x = x + s
+            result = np.row_stack((result, data[x:x + c_w, y:y + c_h].flatten()))
+            print(x,y)
+    return result
 
 images, labels =load_mnist()
 
 result = mnsit_feature_data(7,7,1)
-kmeans = kmeans_feature(result,16)
+kmeans,cluster_centers = kmeans_feature(result,16)
 
-rebuild_mnist(kmeans,np.uint8(images[0], dtype=int).reshape(28, 28),7,7,7)
+
+print(kmeans.predict(rebuild_mnist(kmeans,np.uint8(images[0], dtype=int).reshape(28, 28),7,7,7)))
 #for i in range(kmeans.shape[0]):
 #    display_image(np.uint8(kmeans[i], dtype=int).reshape(7, 7))
