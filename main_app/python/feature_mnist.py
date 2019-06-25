dc607a025282eddb5fed98ab0a4b637f2d8de379
liting <2863515010@qq.com>
2019-06-25 17:22:34 +0800

    update

---
+++ main_app/python/feature_mnist.py
@@ -0,0 +1,38 @@
+from mnist import MNIST
+import datetime as dt
+import numpy as np
+
+def load_mnist():
+    data = MNIST('./python-mnist/data')
+    images, labels = data.load_training()
+    return images, labels
+
+def mnist_to_matrix(data):
+    return np.uint8(data, dtype=int).reshape(28, 28)
+
+#拆分一个矩阵分解为特征矩阵
+def split_feature_data_c(data,c_w,c_h,s):
+    result = np.empty(shape=[0,c_w*c_h], dtype=int)
+    for x in range(0,data.shape[0]-c_w+1,s):
+        for y in range(0,data.shape[1]-c_h+1,s):
+            result = np.vstack((result, data[x:x+c_w,y:y+c_h].flatten()))
+    return result
+
+#拆分一堆矩阵分解为特征
+def split_feature_data(c_w,c_h,s):
+    images, labels = load_mnist()
+    for i in range(0, 60, 1):
+        result = np.empty(shape=[0, c_w * c_h], dtype=int)
+        for j in range(0, 1000, 1):
+            print(dt.datetime.now(), j)
+            result = np.vstack((result, split_feature_data_c(mnist_to_matrix(images[i*1000+j]), c_w, c_h, s)))
+        np.savetxt('./f/feature_'+str(i)+'.gz', result)
+    print("load " + str(0) + " ..")
+    feature = np.loadtxt('./f/feature_' + str(0) + '.gz')
+    for i in range(1, 60, 1):
+        print("load " + str(i) + " ..")
+        feature = np.vstack((feature, np.loadtxt('./f/feature_' + str(i) + '.gz')))
+    return 0
+
+
+split_feature_data(7,7,1) #分解图像
--- main_app/python/index.py
+++ main_app/python/index.py
@@ -1,6 +1,8 @@
 from mnist import MNIST
 from PIL import Image
 from sklearn.cluster import KMeans
+from time import strftime, localtime
+import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
@@ -17,44 +19,52 @@ def display_image(data):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def mnsit_feature_data_c(data,c_w,c_h,s):
+def mnist_to_matrix(data):
+    return np.uint8(data, dtype=int).reshape(28, 28)
+
+#拆分一个矩阵分解为特征矩阵
+def split_feature_data_c(data,c_w,c_h,s):
     result = np.empty(shape=[0,c_w*c_h], dtype=int)
     for x in range(0,data.shape[0]-c_w+1,s):
         for y in range(0,data.shape[1]-c_h+1,s):
-            #print(data[x:x+c_w,y:y+c_h].flatten())
-            result = np.row_stack((result, data[x:x+c_w,y:y+c_h].flatten()))
+            result = np.vstack((result, data[x:x+c_w,y:y+c_h].flatten()))
     return result
 
-def mnsit_feature_data(c_w,c_h,s):
+#拆分一堆矩阵分解为特征
+def split_feature_data(c_w,c_h,s):
     images, labels = load_mnist()
     result = np.empty(shape=[0,c_w*c_h], dtype=int)
-    #for i in range(len(images)):
-    for i in range(10):
-        #print("count:",i)
-        data = np.uint8(images[i], dtype=int).reshape(28, 28)
-        for x in range(0,data.shape[0]-c_w+1,s):
-            for y in range(0,data.shape[1]-c_h+1,s):
-                #print(data[x:x+c_w,y:y+c_h].flatten())
-                result = np.row_stack((result, data[x:x+c_w,y:y+c_h].flatten()))
+    #for i in range(0,len(images),1):
+    for i in range(0, 60, 1):
+        print(dt.datetime.now(),i)
+        result = np.vstack((result, split_feature_data_c(mnist_to_matrix(images[i]),c_w,c_h,s)))
     return result
 
-def kmeans_feature(data,n_class):
+#进行矩阵kmeans处理
+def cpu_kmeans_feature(data,n_class):
     kmeans = KMeans(n_clusters=n_class).fit(data)
-    #y_kmeans = kmeans.predict(data)
     return kmeans,kmeans.cluster_centers_ # kmeans.labels_ # kmeans.inertia_
 
-def rebuild_mnist(kmeans,data,c_w,c_h,s):
-    result = np.empty(shape=[0, c_w * c_h], dtype=int)
-    for x in range(0,data.shape[0] - c_w + 1,s):
-        for y in range(0,data.shape[1] - c_h + 1,s):
-            result = np.row_stack((result, data[x:x + c_w, y:y + c_h].flatten()))
-    return result
+#重构数据
+def rebuild_matrix_data(kmeans,data,c_w,c_h,s):
+    fit_array = kmeans.predict(split_feature_data_c(data, c_w,c_h,s))
+    fit_index = 0
+    for x in range(0,data.shape[0]-c_w+1,s):
+        for y in range(0,data.shape[1]-c_h+1,s):
+            #data[x:x + c_w, y:y + c_h] = np.uint8(kmeans.cluster_centers_[fit_array[fit_index]], dtype=int).reshape(c_w, c_h)
+            data[x:x+c_w,y:y+c_h] = kmeans.cluster_centers_[fit_array[fit_index]].reshape(c_w, c_h)
+            fit_index = fit_index+1
+    return data
 
-images, labels =load_mnist()
+images, labels =load_mnist()#加载数据
+#result = split_feature_data(7,7,1)#分解特征
 
-result = mnsit_feature_data(7,7,1)
-kmeans,cluster_centers = kmeans_feature(result,16)
-
-print(kmeans.predict(mnsit_feature_data_c(np.uint8(images[0], dtype=int).reshape(28, 28),7,7,7)))
-#for i in range(kmeans.shape[0]):
-#    display_image(np.uint8(kmeans[i], dtype=int).reshape(7, 7))
+print("load data..")
+result  = np.loadtxt("./f/feature_0.gz")
+print("kmeans data..")
+kmeans,cluster_centers = cpu_kmeans_feature(result,255)#进行聚类
+print("print data..")
+for i in range(1000):
+    display_image(rebuild_matrix_data(kmeans,mnist_to_matrix(images[10000+i]),7,7,7))
+#np.savetxt('feature.gz', result)
+#np.savetxt('feature.csv', result, delimiter = ',')
