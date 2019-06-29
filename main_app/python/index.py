d07337e2cd19567bfcefd3b47b6309d211147fe8
liting <2863515010@qq.com>
2019-06-29 22:25:21 +0800

    update

--- main_app/python/index.py
+++ main_app/python/index.py
@@ -9,29 +9,24 @@ def load_mnist():
     image, label = data.load_training()
     return image, label
 
-def display_image(data):
-    #display_image(np.uint8(images[0], dtype=int).reshape(28, 28))
-    cv2.imshow('img', data)
+def display_image( data):
+    #cv2.imshow("image", cv2.resize(data, None, fx=10, fy=10, interpolation=cv2.INTER_CUBIC))
+    cv2.imshow("image", cv2.resize(data, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 #采用cosine返回最小距离的特征
-def re_feature_matrix(c_means,data):
-    m_len=0
-    m_class=0
-    for i in range(c_means.shape[0]):
-        ''' 
-        if np.sum(c_means[i]) is 0:
-            n_len=0
-        else:
-            n_len = 0.5+0.5*np.corrcoef(c_means[i],data)[0][1]
-        '''
-        n_len = np.sqrt(np.sum(np.square(c_means[i]-data)))
+def re_feature_matrix(center_data,data):
+    m_class = 0
+    m_len = np.linalg.norm(data-center_data[0])
+    for i in range(1,center_data.shape[0],1):
+        #n_len = 0.5+0.5*np.corrcoef(center_data[i],data)[0][1]
+        n_len = np.linalg.norm(data-center_data[i])
         if n_len < m_len:
             m_len = n_len
             m_class = i
-    return c_means[m_class]
+    return center_data[m_class]
 
 #拆分一个矩阵分解为特征矩阵
 def split_feature_data_c(data,c_w,c_h,s):
@@ -46,18 +41,31 @@ def rebuild_matrix_data(k_means,i_data,c_w,c_h,s):
     data = i_data.copy()
     for x in range(0,data.shape[0]-c_w+1,s):
         for y in range(0,data.shape[1]-c_h+1,s):
-            #display_image(data[x:x + c_w, y:y + c_h])
-            #display_image(re_feature_matrix(k_means,data[x:x + c_w, y:y + c_h].flatten()).reshape(c_w,c_h))
-            data[x:x + c_w, y:y + c_h] = re_feature_matrix(k_means,data[x:x + c_w, y:y + c_h].flatten()).reshape(c_w,c_h)
+            data[x:x + c_w, y:y + c_h] = re_feature_matrix(k_means,data[x:x + c_w, y:y + c_h].flatten().astype('float32')).reshape(c_w,c_h).astype('uint8')
     return data
+
+
+def print_acc_loss(data,re_data):
+    loss=0
+    for i in range(data.shape[0]):
+        loss = loss + np.linalg.norm(data - re_data)
+    return loss/data.shape[0]
 
 images, labels =load_mnist()#加载数据
 
-if os.path.exists("feature_file_L2_512.npy") == False:
+def delete_same_rows(data):
+    new_array = [tuple(row) for row in data]
+    uniques = np.unique(new_array, axis=0)
+    return uniques
+
+f_files = "feature_file_L2_512.npy"
+if os.path.exists(f_files) == False:
     print("not found feature file .npy")
 else:
-    feature = np.load("feature_file_L2_512.npy").astype('uint8')
+    feature = np.load(f_files).astype('uint8')
+    feature = delete_same_rows(feature).astype('float32')
+    print(feature.shape)
 
-for i in range(1000):
-    display_image(rebuild_matrix_data(feature,np.uint8(images[i], dtype=int).reshape(28, 28),7,7,7))
+#for i in range(1000):
+    #display_image(rebuild_matrix_data(feature,np.uint8(images[i+1000], dtype=int).reshape(28, 28),7,7,7))
     #display_image(feature[i].reshape(7, 7))
