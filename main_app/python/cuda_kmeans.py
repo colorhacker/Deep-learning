45a1d5e3a7cb49e1bc7642f48333b6cfbade4a17
liting <2863515010@qq.com>
2019-07-03 17:59:04 +0800

    update

--- main_app/python/cuda_kmeans.py
+++ main_app/python/cuda_kmeans.py
@@ -10,7 +10,7 @@ def kmeans_data(i_file,o_file,c_array):
         centroids, assignments = kmeans_cuda(feature, c_array[i], init="random", yinyang_t=0, verbosity=1)
         np.save(o_file+str(c_array[i]), centroids)
 
-kmeans_data('./split_feature/split_feature_7x7x1_t.npy',"./kmeans_feature_7x7x1/f_t_",[8,16,32,64,128,256,512,768,1024,2048,4096,10240,20480])
+#kmeans_data('./split_feature/split_feature_7x7x1_t.npy',"./kmeans_feature_7x7x1/f_t_",[8,16,32,64,128,256,512,768,1024,2048,4096,10240,20480])
 kmeans_data('./split_feature/split_feature_7x7x2_t.npy',"./kmeans_feature_7x7x2/f_t_",[8,16,32,64,128,256,512,768,1024,2048,4096,10240,20480])
 kmeans_data('./split_feature/split_feature_7x7x3_t.npy',"./kmeans_feature_7x7x3/f_t_",[8,16,32,64,128,256,512,768,1024,2048,4096,10240,20480])
 kmeans_data('./split_feature/split_feature_7x7x4_t.npy',"./kmeans_feature_7x7x4/f_t_",[8,16,32,64,128,256,512,768,1024,2048,4096,10240,20480])
