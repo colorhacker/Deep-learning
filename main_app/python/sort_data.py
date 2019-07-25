7bf0e6c2601a72d8f4912b74edf1b57812dab004
liting <2863515010@qq.com>
2019-07-25 15:03:16 +0800

    update

--- main_app/python/sort_data.py
+++ main_app/python/sort_data.py
@@ -1,42 +1,53 @@
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 import numpy as np
 
 def kmeans_process(n_class,data):
     kmeans = KMeans(n_clusters=n_class,init='random',algorithm='full',random_state=99).fit(data)
     return kmeans.cluster_centers_, kmeans.labels_
 
-def custum_sort_matrix(data):
+
+def sort_func(x,y):
+    return np.linalg.norm(x - y)
+
+
+def custum_sort_matrix(data, rule=False):
+    target_data = np.zeros(data.shape[1])
     data_list = data.tolist()
-    #return np.array(sorted(data_list, key=lambda x:np.linalg.norm(np.zeros(data.shape[1]) - np.array(x))))
-    #return np.array(sorted(range(len(data_list)), key=lambda x:np.linalg.norm(np.zeros(data.shape[1]) - np.array(data_list[x]))))
-    value = np.array(sorted(data_list, key=lambda x:np.linalg.norm(np.zeros(data.shape[1]) - np.array(x))))
-    labels = np.array(sorted(range(len(data_list)), key=lambda x:np.linalg.norm(np.zeros(data.shape[1]) - np.array(data_list[x]))))
+    if rule == True:
+        # target_data = np.array(sorted(data_list, key=lambda element: sort_func(target_data , np.array(element))))
+        target_data = np.array(sorted(data_list, key=lambda element: sort_func(target_data , np.array(element))))[len(data_list)-1]
+    value = np.array(sorted(data_list, key=lambda element: sort_func(target_data , np.array(element))))
+    labels = np.array(sorted(range(len(data_list)), key=lambda element: sort_func(target_data , np.array(data_list[element]))))
     return value,labels
 
 
-def kmeans_sort(n_class,data):
+def kmeans_sort(n_class, data):
     k_center, k_label = kmeans_process(n_class, data)
-    k_sort_center, k_sort_label = custum_sort_matrix(k_center)
+    k_sort_center, k_sort_label = custum_sort_matrix(k_center,rule=True)
     list_data = {}
     for i in range(n_class):
         list_data[i]=[]
         for j in range(data.shape[0]):
             if i == k_label[j]:
                 list_data[i].append(data[j])
-    # k_center.take(k_sort_label)
+        list_data[i], _ = custum_sort_matrix(np.array(list_data[i]),rule=True) #最小元素排序
     sort_data = [list_data[i] for i in k_sort_label]
-    array_data=np.empty(shape=[0, data.shape[1]])
+    ret_data = np.empty(shape=[0, data.shape[1]])
     for i in range(len(sort_data)):
-        array_data = np.vstack((array_data,sort_data[i]))
-    return array_data
+        ret_data = np.vstack((ret_data,sort_data[i]))
+    return ret_data
 
 
 if __name__=='__main__':
-    src_data = np.random.randn(100,1)
-    # src_data = np.linspace(-1,100,100).reshape(100,1)
+    src_data = np.random.randn(10000,2)
+    # np.random.shuffle(src_data)
     res_data = kmeans_sort(10,src_data)
-    print(src_data)
-    print(res_data)
 
+    plt.plot(np.arange(0, res_data.shape[0]), res_data, alpha=0.5)
+    plt.show()
+    # a,b=custum_sort_matrix(src_data, rule=True)
+    # plt.plot(np.arange(0, res_data.shape[0]), a, alpha=0.5)
+    # plt.show()
 
 
