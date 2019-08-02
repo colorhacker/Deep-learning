7603faf729ff29977c4257338f34099e70a27984
liting <2863515010@qq.com>
2019-08-02 19:27:38 +0800

    update

--- main_app/python/neurons.py
+++ main_app/python/neurons.py
@@ -5,6 +5,7 @@ from random import randint,shuffle
 class Dendrites:
     def __init__(self, synapse, length):
         self.spine = []
+        self.energy = 0
         self.count = randint(1,length)
         for i in range(self.count):
             self.spine.append([0]*randint(1,int(self.count**0.5)))
@@ -13,19 +14,19 @@ class Dendrites:
         for i in range(len(self.spine)):
             self.spine[i].insert(0,self.spine[i].pop()) #向右移动一格
             self.spine[i][0] = 0 #清除最左边值
-
+            self.energy = self.energy + self.spine[i][-1]
 
 class Neurons:
     def __init__(self, dendrites, axon):
-        self.dendrites = dendrites #神经元后级树突
+        self.dendrites = dendrites #神经元前级树突
         self.energy = 0  #神经元中心的能量值
         self.threshold = 0 #神经元中心的阈值
         self.axon = axon #神经元后级轴突
 
     def cell_tick(self):
-        if sum(self.dendrites) > self.neurons_cell_threshold:
-            self.neurons_cell_energy = sum(self.dendrites)
-        self.neurons_cell_threshold = self.neurons_cell_energy**0.5
+        if sum(self.dendrites.energy) > self.threshold:
+            self.energy = sum(self.dendrites)
+        self.threshold = self.energy**0.5
 
 
 class Axon:
