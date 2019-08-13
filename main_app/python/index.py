bf4fb5a60e348eb0c28f9845a0926fea1c6190d3
litinghello <2863515010@qq.com>
2019-08-13 17:29:12 +0800

    update

--- main_app/python/index.py
+++ main_app/python/index.py
@@ -8,7 +8,7 @@ from random import randint, shuffle, seed, choice
 from tqdm import tqdm
 
 
-def serial_training(net_model, train_data):
+def serial_process(net_model, train_data):
     for a in train_data:
         net_model.batch_tick(a)
         plt.plot(net_model.active_freq())
@@ -16,11 +16,12 @@ def serial_training(net_model, train_data):
     plt.show()
 
 
-def parallel_training(net_model, train_data):
+def parallel_process(net_model, train_data):
     try:
         pool = Pool(len(train_data))
         result = list([[]]*len(train_data))
         for i, e in enumerate(train_data):
+            # np.random.shuffle(e)
             result[i] = pool.apply_async(func=net_model.batch_tick, args=(e,))
         pool.close()
         pool.join()
@@ -34,7 +35,9 @@ def parallel_training(net_model, train_data):
 if __name__ == '__main__':
     seed(0)
     # 神经元个数，输入个数，树突最小长度，树突个数，突触长度，突触抑制率
-    model = N.Networks(100, 784, 5, 100, 10, 0.2)
+    model = N.Networks(100, 784, 5, 20, 10, 0.2)
     mnist_train = np.load("./temp/mnist_train.npy")
-    parallel_training(model, mnist_train)
-    # serial_training(model, mnist_train)
+    mnist_test = np.load("./temp/mnist_test.npy")
+    parallel_process(model, mnist_test)
+    # parallel_process(model, mnist_train)
+    # serial_process(model, mnist_train)
